110b4652ce28ac696611728ecea2e0178631d60a
Michael David <lordkay07@gmail.com>
2022-06-02 21:23:48 -0700

    write a C program that prints with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-
+/*
+ *main is the code entry point
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
