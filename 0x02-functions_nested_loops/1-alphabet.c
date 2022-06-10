91ddbe4de9e4538d6415378306e11e4a4a3e985c
Michael David <lordkay07@gmail.com>
2022-06-10 15:32:00 -0700

    Alphabet + Insomnia pain

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabets in lowercase
+ *
+ * Return: 0 (Always Success)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
