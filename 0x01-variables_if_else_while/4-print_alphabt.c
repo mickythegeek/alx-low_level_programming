fb9b2df0a6d61634de40403d8597297e53979146
Michael David <lordkay07@gmail.com>
2022-06-09 20:59:55 -0700

    prnt all letters of alphabet except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry Point
+ *
+ * letter - For alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' && letter == 'e')
+			letter = '';
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
