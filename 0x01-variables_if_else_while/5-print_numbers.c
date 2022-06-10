c51ac385221d68e72928bcd92c94965a5bc723c7
Michael David <lordkay07@gmail.com>
2022-06-09 21:10:52 -0700

    program that prints all single digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * digit - Representing digits from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(voi)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf('\n');
+	return (0);
+}
