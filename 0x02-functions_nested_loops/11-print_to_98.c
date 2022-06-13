23534120eff381369960239f4937ad19af66ca9b
Michael David <lordkay07@gmail.com>
2022-06-13 04:44:53 -0700

    function that prints all natral numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,7 @@ void print_to_98(int n)
 		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
+
 		}
 	}
 	else if (n < 98)
@@ -23,5 +24,5 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	printf("%d, ", n);
+	printf("%d\n", n);
 }
