7f6fae16d5876b1dc806f9260a80512cf6e7461e
Michael David <lordkay07@gmail.com>
2022-06-15 15:01:54 -0700

    write a function that checks for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,13 @@
 /**
  * _isdigit - Checks for a digit
  *
- * @c: Input character
+ * @c: input character
  *
- * Return: 1, if c is a digit. 0, if c is not
+ * Return: 1, if c is digit. 0, if c is not
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <=57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
-}
