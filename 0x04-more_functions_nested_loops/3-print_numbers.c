91ca40823933a886de2bf3af92248bd607c6b418
Michael David <lordkay07@gmail.com>
2022-06-15 22:46:58 -0700

    print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_numbers(void)
 {
-	char ch;
+	char c = '0';
 	
-	for (ch = '0'; ch <= '9'; ch++)
+	for (; c <= '9'; c++)
 	{
-		_putchar(ch);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
