c7add3ec7cc557af726985d2aafdfebea90390e1
Michael David <lordkay07@gmail.com>
2022-06-22 23:33:10 -0700

    prints string skipping odd indexes

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - print string skipping odd index
+ * @str: given string
+ */
+void puts2(char *str)
+{
+	int x = 0;
+	int y = 0;
+
+	while (*(str + x) != 0)
+	{
+		x++;
+	}
+	while (y < x)
+	{
+		_putchar(*(str + y));
+		y = y + 2;
+	}
+	_putchar('\n');
+}
