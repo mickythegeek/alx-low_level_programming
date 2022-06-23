599848d8c915bfc1e2eb09013074af1bfa19e736
Michael David <lordkay07@gmail.com>
2022-06-22 22:54:23 -0700

    prints a string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a given string stdout
+ * @str: given string
+ */
+void _puts(char *str)
+{
+	int s = 0;
+
+	while (*(str + s) != 0)
+	{
+		_putchar(*(str + x));
+		s++;
+	}
+	_putchar('\n');
+}
