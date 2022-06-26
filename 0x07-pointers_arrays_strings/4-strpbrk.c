70b8f095a5cb88c81a1587334b3e9758b68b5dc6
Michael David <lordkay07@gmail.com>
2022-06-26 14:14:14 -0700

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,11 +11,11 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\0'; j++;)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (a[i] == accept[j])
+			if (s[i] == accept[j])
 				return s[i];
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
