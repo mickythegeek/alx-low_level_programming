1e1a5026d9f8e03c6261d7944b263d8c3f6061e0
Michael David <lordkay07@gmail.com>
2022-06-27 05:12:22 -0700

    returns the factorial of a given number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: base number
- * *y: exponent
+ * @y: exponent
  * Return: value of the exponentiation
  */
 int _pow_recursion(int x, int y)
