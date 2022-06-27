d90cd07780a034dbcc0994c6ee39d14a1e4c1c0e
Michael David <lordkay07@gmail.com>
2022-06-27 06:04:12 -0700

    returns 1 if a s tring is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,7 +17,7 @@ int _strlen_recursion(char *s)
  * @s: string
  * @n1: smallest iterator
  * @n2: biggest iterator
- * Return: something 
+ * Return: somethin
  */
 int comparator(char *s, int n1, int n2)
 {
