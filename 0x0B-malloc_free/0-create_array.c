28afabf07fc8370f5642b953e9888f0d0a3bbe70
Michael David <lordkay07@gmail.com>
2022-07-04 23:43:35 -0700

    creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - makes an array of dynamic memory
+ * @size: size of array
+ * @c: arg to initialze first index with
+ * Return: Pointer to array or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+	
+	while (1)
+	{
+		p = malloc(sizeof(char) * size));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < size; i++)
+		{
+			p[i] = c;
+		}
+		if (size == 0)
+			return (NULL);
+		return (p);
+
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Automatic and Dynamic Allocation of Memory
