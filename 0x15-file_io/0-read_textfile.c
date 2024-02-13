5c4878d0f429489a08d7fdade7bb3ca4ee5c8236
Michael David <lordkay07@gmail.com>
2024-02-13 08:37:32 -0800

    function that reads a textfile and prints it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,5 +11,30 @@
 
 ssize_t read_file(const char *filename, size_t letters)
 {
-	unsigned int u_int;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if(!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	
+	if (!buf)
+		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+
+}
 	
