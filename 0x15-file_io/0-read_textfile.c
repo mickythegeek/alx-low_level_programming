20b90aaabc93d47a414dda76c7d14d1368f0b343
Michael David <lordkay07@gmail.com>
2024-02-19 07:37:59 -0800

    fixing the header files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include <fnctl.h>
 /**
  * read_textfile - function that reads a text_file and prints to POSIX stdout
  * @letters: num of letters it should read and print
