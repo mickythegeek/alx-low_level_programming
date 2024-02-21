a36e0ae8457ba7a68c606fb6f5d4c12fd728e07d
Michael David <lordkay07@gmail.com>
2024-02-21 16:52:32 +0100

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 
 
 
-	bytes_written = write(fd, text_content, nletters));
+	bytes_written = write(fd, text_content, nletters);
 
 	if (bytes_written == -1)
 		return (-1);
