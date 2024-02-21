de73de8cab23f09219b482e39edc8439964549af
Michael David <lordkay07@gmail.com>
2024-02-21 15:48:28 +0100

    implement a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * Return: 1, if successful, -1 if filename is NULL
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t nrd, nwr;
