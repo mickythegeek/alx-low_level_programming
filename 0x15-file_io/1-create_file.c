d4eac8a44bf2b48515caa66d5b54a29504c44315
Michael David <lordkay07@gmail.com>
2024-02-21 16:07:24 +0100

    implement a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (0);
 
-	fd = open(filename, o_CREAT | O_RDONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
 	text_content = "God, when?";
 	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
 
