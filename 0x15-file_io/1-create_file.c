8f6878a6fe1c405fdee17a45a39603eb73d1b6f6
Michael David <lordkay07@gmail.com>
2024-02-21 16:44:58 +0100

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	if (!text_content)
 		text_content = "";
